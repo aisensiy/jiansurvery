0b8cba6d7d3700999a5bbfba728cdbd2ea98dc64
aisensiy <aisensiy@gmail.com>
2013-05-07 10:41:28 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jiansurvery::Application.config.secret_token = '8f6ac09159544beaab33821918c977de194fd92da9f819a683e92c90f1ae7f8bec0a0768c09072d99184effc3728bf94ca9886d24cf4394fb7d70fa543eb3d10'
